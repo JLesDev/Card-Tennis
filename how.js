84d334a4d69ffdf8be0111148c1bc38f83ba6881
JLesDev <jontyleslie@gmail.com>
2026-01-03 21:42:44 +1100

    Adding tutorial

---
+++ how.js
@@ -0,0 +1,14 @@
+document.getElementById('melb').addEventListener('click', function (e) {
+    // runner(0)
+    window.location.href = 'index.html'
+})
+
+document.getElementById('adel').addEventListener('click', function (e) {
+    // runner(1)
+    window.location.href = 'how.html'
+})
+
+document.getElementById('more').addEventListener('click', function (e) {
+    // runner(3)
+    window.location.href = 'more.html'
+})
